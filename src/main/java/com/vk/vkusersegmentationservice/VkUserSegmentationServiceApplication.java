d76e6c4f34f846568bb7c164ceedde65b4db4141
Eljan Najafov <eljan.najafov02@mail.ru>
2025-03-27 11:30:07 +0400

    init

---
+++ src/main/java/com/vk/vkusersegmentationservice/VkUserSegmentationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vk.vkusersegmentationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkUserSegmentationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VkUserSegmentationServiceApplication.class, args);
+    }
+
+}
