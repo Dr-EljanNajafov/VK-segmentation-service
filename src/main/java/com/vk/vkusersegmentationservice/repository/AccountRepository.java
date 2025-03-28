9d8e0443f224e1f4ed1fb18dc5f4a91492277969
Eljan Najafov <eljan.najafov02@mail.ru>
2025-03-28 15:29:41 +0400

    adding account & repository

---
+++ src/main/java/com/vk/vkusersegmentationservice/repository/AccountRepository.java
@@ -0,0 +1,13 @@
+package com.vk.vkusersegmentationservice.repository;
+
+import com.vk.vkusersegmentationservice.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findByEmail(String email);
+}
