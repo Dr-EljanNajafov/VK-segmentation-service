861c82b7a7dcb132fbdcebd0eb9d532aeb958d3d
Eljan Najafov <eljan.najafov02@mail.ru>
2025-03-28 15:32:36 +0400

    removed bugs in security logic

--- src/main/java/com/vk/vkusersegmentationservice/VkUserSegmentationServiceApplication.java
+++ src/main/java/com/vk/vkusersegmentationservice/VkUserSegmentationServiceApplication.java
@@ -2,8 +2,12 @@ package com.vk.vkusersegmentationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true,securedEnabled = true)
 public class VkUserSegmentationServiceApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/vk/vkusersegmentationservice/controller/AccountController.java
@@ -0,0 +1,87 @@
+package com.vk.vkusersegmentationservice.controller;
+
+import com.vk.vkusersegmentationservice.constants.ApplicationConstants;
+import com.vk.vkusersegmentationservice.model.Account;
+import com.vk.vkusersegmentationservice.model.dto.LoginRequestDTO;
+import com.vk.vkusersegmentationservice.model.dto.LoginResponseDTO;
+import com.vk.vkusersegmentationservice.repository.AccountRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.GrantedAuthority;
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class AccountController {
+
+
+    private final AccountRepository accountRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final Environment env;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody Account account) {
+        try {
+            String hashPwd = passwordEncoder.encode(account.getPassword());
+            account.setPassword(hashPwd);
+            Account savedAccount = accountRepository.save(account);
+
+            if (savedAccount.getId() > 0) {
+                return ResponseEntity.status(HttpStatus.CREATED).
+                        body("Given user details are successfully registered");
+            } else {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).
+                        body("User registration failed");
+            }
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).
+                    body("An exception occurred: " + ex.getMessage());
+        }
+    }
+
+    @RequestMapping("/user")
+    public Account getUserDetailsAfterLogin(Authentication authentication) {
+        Optional<Account> optionalCustomer = accountRepository.findByEmail(authentication.getName());
+        return optionalCustomer.orElse(null);
+    }
+
+    @PostMapping("/ApiLogin")
+    public ResponseEntity<LoginResponseDTO> apiLogin (@RequestBody LoginRequestDTO loginRequest) {
+        String jwt = "";
+        Authentication authentication = UsernamePasswordAuthenticationToken.unauthenticated(loginRequest.username(),
+                loginRequest.password());
+        Authentication authenticationResponse = authenticationManager.authenticate(authentication);
+        if(null != authenticationResponse && authenticationResponse.isAuthenticated()) {
+            if (null != env) {
+                String secret = env.getProperty(ApplicationConstants.JWT_SECRET_KEY,
+                        ApplicationConstants.JWT_SECRET_DEFAULT_VALUE);
+                SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+                jwt = Jwts.builder().issuer("vk").subject("JWT Token")
+                        .claim("username", authenticationResponse.getName())
+                        .claim("authorities", authenticationResponse.getAuthorities().stream().map(
+                                GrantedAuthority::getAuthority).collect(Collectors.joining(",")))
+                        .issuedAt(new java.util.Date())
+                        .expiration(new java.util.Date((new java.util.Date()).getTime() + 30000000))
+                        .signWith(secretKey).compact();
+            }
+        }
+        return ResponseEntity.status(HttpStatus.OK).header(ApplicationConstants.JWT_HEADER,jwt)
+                .body(new LoginResponseDTO(HttpStatus.OK.getReasonPhrase(), jwt));
+    }
+}
